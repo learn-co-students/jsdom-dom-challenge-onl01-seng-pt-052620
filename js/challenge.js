95b3dff3b3afbb4d720f1bb1571aaddeed220e4f
wynterwolf <shinyguns@gmail.com>
2020-11-30 12:30:05 -0600

    commit tree 6117d14281391a0223bb7b0d43dbdad6e31e2f9c

--- js/challenge.js
+++ js/challenge.js
@@ -1,1 +1,12 @@
-// comment here
+// what you need
+let counter_id = document.getElementById("counter")
+
+let counter = 0
+// timer increment per second
+let timer = setInterval(function () {
+    if (true = true) {
+        counter_id.innerHTML = counter
+        counter += 1;
+    }
+}, 1000);
+
